dd431a5b2242d7b3c08a44af3ff6a605bb75bc94
Richard Okoro <glasshouse747@gmail.com>
2025-04-15 15:20:55 +0100

    test commit

--- frontend.sh
+++ frontend.sh
@@ -3,3 +3,8 @@ dnf module enable nginx:1.24 -y
 dnf install nginx -y
 systemctl enable nginx
 systemctl start nginx
+cp nginx.conf /etc/nginx/nginx.conf
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+systemctl restart nginx
