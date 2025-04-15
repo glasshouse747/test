03bee99d5d629fc409fd7d487f8a74760416e225
Richard Okoro <glasshouse747@gmail.com>
2025-04-15 15:20:55 +0100

    test commit

---
+++ catalogue.sh
@@ -0,0 +1,14 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+cp catalogue.service /etc/systemd/system/catalogue.service
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+npm install
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
