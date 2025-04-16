23982c3e6b33b167deeb221bd70064c77f4a23f3
Richard Okoro <glasshouse747@gmail.com>
2025-04-16 22:17:22 +0100

    new test commit

--- user.sh
+++ user.sh
@@ -8,6 +8,7 @@ mkdir /app
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
 cd /app
 unzip /tmp/user.zip
+cd /app
 npm install
 systemctl daemon-reload
 systemctl enable user
