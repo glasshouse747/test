1a37b5df2816b08bcacb545dfc93cfb463e95d84
Richard Okoro <glasshouse747@gmail.com>
2025-04-16 21:19:20 +0100

    new test commit

--- redis.sh
+++ redis.sh
@@ -1,6 +1,6 @@
 dnf module disable redis -y
 dnf module enable redis:7 -y
 dnf install redis -y
-sed -e 'i/127.0.0.1/0.0.0.0/g' -e 's/protected-mode yes/protected-mode no' /etc/redis/redis.conf
+sed -e '/127.0.0.1/c 0.0.0.0/g' -e '/protected-mode yes/c protected-mode no' /etc/redis/redis.conf
 systemctl enable redis
 systemctl start redis
