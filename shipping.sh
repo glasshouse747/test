04083a0fa983b0e83d12e5d15e13ed82c1df9077
Richard Okoro <glasshouse747@gmail.com>
2025-04-17 00:48:53 +0100

    new test commit

--- shipping.sh
+++ shipping.sh
@@ -15,3 +15,4 @@ dnf install mysql -y
 mysql -h <mysql-dev.mydevops.shop> -uroot -pRoboShop@1 < /app/db/schema.sql
 mysql -h <mysql-dev.mydevops.shop> -uroot -pRoboShop@1 < /app/db/app-user.sql
 mysql -h <mysql-dev.mydevops.shop> -uroot -pRoboShop@1 < /app/db/master-data.sql
+systemctl restart shipping
