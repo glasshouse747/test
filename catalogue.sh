54c63d92068656c7b3b276f45ea4af9d87e01c61
Richard Okoro <glasshouse747@gmail.com>
2025-04-16 20:36:15 +0100

    new test commit

--- catalogue.sh
+++ catalogue.sh
@@ -2,6 +2,7 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 useradd roboshop
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
@@ -9,6 +10,9 @@ cd /app
 unzip /tmp/catalogue.zip
 cd /app
 npm install
+dnf install mongodb-mongosh -y
+mongosh --host mongodb-dev.mydevops.shop </app/db/master-data.js
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
+
