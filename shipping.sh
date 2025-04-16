71eab40cfd2879011186b7bf059f13048f1fa9db
Richard Okoro <glasshouse747@gmail.com>
2025-04-17 00:18:06 +0100

    new test commit

--- shipping.sh
+++ shipping.sh
@@ -1,11 +1,11 @@
 dnf install maven -y
 cp shipping.service /etc/systemd/system/shipping.service
+rm -rf /app
 useradd roboshop
 mkdir /app
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip
 cd /app
 unzip /tmp/shipping.zip
-cd /app
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 dnf install mysql -y
