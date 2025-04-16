25ba60ed82d8607c5f29b4c8285aaf3f60e0ccea
Richard Okoro <glasshouse747@gmail.com>
2025-04-17 00:49:43 +0100

    new test commit

--- shipping.sh
+++ shipping.sh
@@ -12,7 +12,7 @@ systemctl daemon-reload
 systemctl enable shipping
 systemctl start shipping
 dnf install mysql -y
-mysql -h <mysql-dev.mydevops.shop> -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h <mysql-dev.mydevops.shop> -uroot -pRoboShop@1 < /app/db/app-user.sql
-mysql -h <mysql-dev.mydevops.shop> -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h mysql-dev.mydevops.shop -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h mysql-dev.mydevops.shop -uroot -pRoboShop@1 < /app/db/app-user.sql
+mysql -h mysql-dev.mydevops.shop -uroot -pRoboShop@1 < /app/db/master-data.sql
 systemctl restart shipping
