d1d7cbbd11c599e237e889a9647e3b1b68bb313e
Richard Okoro <glasshouse747@gmail.com>
2025-04-17 00:22:22 +0100

    new test commit

--- shipping.sh
+++ shipping.sh
@@ -8,10 +8,10 @@ cd /app
 unzip /tmp/shipping.zip
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
+systemctl daemon-reload
+systemctl enable shipping
+systemctl start shipping
 dnf install mysql -y
 mysql -h <mysql-dev.mydevops.shop > -uroot -pRoboShop@1 < /app/db/schema.sql
 mysql -h <mysql-dev.mydevops.shop > -uroot -pRoboShop@1 < /app/db/app-user.sql
 mysql -h <mysql-dev.mydevops.shop > -uroot -pRoboShop@1 < /app/db/master-data.sql
-systemctl daemon-reload
-systemctl enable shipping
-systemctl start shipping
